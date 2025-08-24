b89750b1dc51bd8b7ef552ae0418ed62795972e4
MUKESHV17 <mukesh631701@gmail.com>
2025-08-24 21:46:42 +0530

    Initial commit - Spring Boot backend

--- src/main/java/com/examly/springapp/config/CorsConfig.java
+++ src/main/java/com/examly/springapp/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("https://e-wallet-management-system.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                         .allowedHeaders("*")
                         .allowCredentials(true)
